95786b75525685ab9a16fab4104324a854f3dafd
manue <manue@DESKTOP-N2RVPRR>
2022-12-08 08:05:43 -0600

    Add project files.

---
+++ MJL_Ecommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MJL_Ecommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
